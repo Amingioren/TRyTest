bff6eaa054c36776b6f178ab40f3c08fb7abc709
Amingioren <anas.mibh@gmail.com>
2024-08-05 10:02:58 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 PORT = 3000;
 IO_PORT = 4000;
-DB_URI = "mongodb://localhost:27017/appoty";
+DB_URI = "mongodb+srv://anasmibh:<cfhaE9jE1ftGJbeQ>@cluster0.dehbopq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 HASH_KEY = "meisam1234$%^!@@";
 BASE_URL = `http://192.168.43.168:${PORT}/`;
 IO_URL = `http://192.168.43.168:${IO_PORT}`;
